1ba52dd5bda2bf029b5cbdff393c55d6e58f21fc
Chaem0215 <lcm836477@gmail.com>
2023-06-19 07:12:39 +0900

    23.06.19

---
+++ Baekjoon/두_배열의_합.py
@@ -0,0 +1,6 @@
+# 골드3
+# 두 배열의 합
+# https://www.acmicpc.net/problem/2143
+# 23.06.17
+
+import sys
--- Baekjoon/먹을_것인가_먹힐_것인가.py
+++ Baekjoon/먹을_것인가_먹힐_것인가.py
@@ -4,3 +4,19 @@
 # 23.06.17
 
 import sys
+
+def toPointer(A, B, start, end):
+
+    return result
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    result = 0
+    N, M = map(int, sys.stdin.readline().split())
+    A = list(map(int, sys.stdin.readline().split()))
+    B = list(map(int, sys.stdin.readline().split()))
+
+    result = toPointer(A, B, 0, 0)
+
+    print(result)
