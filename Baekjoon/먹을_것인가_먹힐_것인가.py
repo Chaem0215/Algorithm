ee7904704f3bcb00d56501b757cbd1d3f396c3f8
Chaem0215 <lcm836477@gmail.com>
2023-06-19 13:33:14 +0900

    23.06.19

--- Baekjoon/먹을_것인가_먹힐_것인가.py
+++ Baekjoon/먹을_것인가_먹힐_것인가.py
@@ -5,18 +5,44 @@
 
 import sys
 
-def toPointer(A, B, start, end):
-
-    return result
+def toPointer(i, start, B):
+    if start >= len(B): return len(B)
+    if i <= B[start]: return len(B) - (len(B) - start)
+    else: return toPointer(i, start + 1, B)
 
 T = int(sys.stdin.readline())
 
 for _ in range(T):
     result = 0
     N, M = map(int, sys.stdin.readline().split())
-    A = list(map(int, sys.stdin.readline().split()))
-    B = list(map(int, sys.stdin.readline().split()))
+    A = sorted(list(map(int, sys.stdin.readline().split())))
+    B = sorted(list(map(int, sys.stdin.readline().split())))
 
-    result = toPointer(A, B, 0, 0)
+    for i in A:
+        result += toPointer(i, 0, B)
 
     print(result)
+    
+    
+# 1. 런타임 에러
+# B의 길이는 최대 20,000개인데 파이썬 default 재귀 길이를 초과해서 런타임에러 뜬거같음.
+
+# import sys
+
+# def toPointer(i, start, B):
+#     if start >= len(B): return len(B)
+#     if i <= B[start]: return len(B) - (len(B) - start)
+#     else: return toPointer(i, start + 1, B)
+
+# T = int(sys.stdin.readline())
+
+# for _ in range(T):
+#     result = 0
+#     N, M = map(int, sys.stdin.readline().split())
+#     A = sorted(list(map(int, sys.stdin.readline().split())))
+#     B = sorted(list(map(int, sys.stdin.readline().split())))
+
+#     for i in A:
+#         result += toPointer(i, 0, B)
+
+#     print(result)
--- Programmers/완주하지_못한_선수.py
+++ Programmers/완주하지_못한_선수.py
@@ -8,9 +8,18 @@ from collections import Counter
 participant = ["leo", "kiki", "eden"]
 completion = ["eden", "kiki"]
 
+# 2. list 이용
 def solution(participant, completion):
     result = Counter(participant) - Counter(completion)
-
     return list(result.keys())[0]
-
+ 
 print(solution(participant, completion))
+
+
+# 1. for 문 이용
+from collections import Counter
+def solution(participant, completion):
+    result = ''
+    for i in (Counter(participant) - Counter(completion)):
+        result = i
+    return result
